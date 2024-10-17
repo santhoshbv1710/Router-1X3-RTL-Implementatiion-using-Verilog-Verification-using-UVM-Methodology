e10cd9875556fa7de7a254e434b62792d7b949c7
SANTHOSH B V <135435798+santhoshbv1710@users.noreply.github.com>
2024-10-17 19:15:01 +0530

    Update rd_if.sv

--- rtl/rd_if.sv
+++ rtl/rd_if.sv
@@ -1,33 +1,25 @@
-interface wr_if (input bit clock);
+interface rd_if (input bit clock);
 
-	bit clk;
+	logic [7:0] data_out;
+	bit read_enb;
+	bit vld_out;
 	
+	bit clk;
 	assign clk = clock;
 
-	bit resetn;
-	bit pkt_valid;
-	logic [7:0] data_in;
-	bit error;
-	bit busy;
-	
-	clocking wr_drv_cb @(posedge clk);
-		default input #1 output #1;
-		output resetn;
-		output data_in;
-		output pkt_valid;
-		input busy;
+	clocking rd_drv_cb @(posedge clk);
+	default input #1 output #1;
+		input vld_out;
+		output read_enb;
 	endclocking
 
-	clocking wr_mon_cb @(posedge clk);
-		default input #1 output #1;
-		input data_in;
-		input pkt_valid;
-		input resetn;
-		input busy;
-		input error;
+	clocking rd_mon_cb @(posedge clk);
+	default input #1 output #1;
+		input data_out;
+		input read_enb;
 	endclocking
-	
-	modport WR_DRV (clocking wr_drv_cb);
-	modport WR_MON (clocking wr_mon_cb);
 
-endinterface		
+	modport RD_DRV (clocking rd_drv_cb);
+	modport RD_MON (clocking rd_mon_cb);
+
+endinterface 
